746295d8614f557b4cfa5deebfed66f77fcda24d
Carmin Kern <38145343+CarminKern@users.noreply.github.com>
2018-08-14 23:24:24 +0200

    Verbesserung der pinset Funktion

--- HD44780.h/HD44780.c
+++ HD44780.h/HD44780.c
@@ -12,11 +12,26 @@
 #include "HD44780.h"
 
 /*--- #defines ------------------------------------------------------ */
+#define DEFAULT_PINSET_EN 22
+#define DEFAULT_PINSET_RS 17
+#define DEFAULT_PINSET_RW 27
+#define DEFAULT_PINSET_D7 21
+#define DEFAULT_PINSET_D6 20
+#define DEFAULT_PINSET_D5 16
+#define DEFAULT_PINSET_D4 12
+#define DEFAULT_PINSET_D3 1
+#define DEFAULT_PINSET_D2 7
+#define DEFAULT_PINSET_D1 8
+#define DEFAULT_PINSET_D0 25
+
+#define DEFAULT_FUNCTIONSET_DL 1
+#define DEFAULT_FUNCTIONSET_N 1
+#define DEFAULT_FUNCTIONSET_F 0
 
 /*--- typedef und Variablen------------------------------------------ */
-typedefstruct_pinset pinset = { 0,0,0,0,0,0,0,0,0,0,0 };
+typedefstruct_pinset pinset = { DEFAULT_PINSET_EN , DEFAULT_PINSET_RS , DEFAULT_PINSET_RW , DEFAULT_PINSET_D7 , DEFAULT_PINSET_D6 , DEFAULT_PINSET_D5 , DEFAULT_PINSET_D4 , DEFAULT_PINSET_D3 , DEFAULT_PINSET_D2 , DEFAULT_PINSET_D1 , DEFAULT_PINSET_D0  };
+typedefstruct_functionset functionset = { DEFAULT_FUNCTIONSET_DL , DEFAULT_FUNCTIONSET_N , DEFAULT_FUNCTIONSET_F };
 
-typedefstruct_funktionset funktionset;
 
 /*--- getVar ---------------------------------------------------------*/
 
@@ -26,107 +41,103 @@ typedefstruct_funktionset funktionset;
 
 int main(void) {
 	printf("\n Starte test...\n\n");
-
-	HD44780_initialize(10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 4,0,1);
-
+	printf("\nEN %d RS %d RW %d D7 %d D6 %d D5 %d D4 %d D3 %d D2 %d D1 %d D0 %d", pinset.EN, pinset.RS, pinset.RW, pinset.D7, pinset.D6, pinset.D5, pinset.D4, pinset.D3, pinset.D2, pinset.D1, pinset.D0);
+	HD44780_initialize(10,9,8,7,6,5,4,3,3,1,0,8,2,10);
+	printf("\nEN %d RS %d RW %d D7 %d D6 %d D5 %d D4 %d D3 %d D2 %d D1 %d D0 %d", pinset.EN, pinset.RS, pinset.RW, pinset.D7, pinset.D6, pinset.D5, pinset.D4, pinset.D3, pinset.D2, pinset.D1, pinset.D0);
+	HD44780_initialize(10, 9, 8, 7, 6, 5, 4, 3, 3, 1, 0, 4, 2, 10);
+	printf("\nEN %d RS %d RW %d D7 %d D6 %d D5 %d D4 %d D3 %d D2 %d D1 %d D0 %d", pinset.EN, pinset.RS, pinset.RW, pinset.D7, pinset.D6, pinset.D5, pinset.D4, pinset.D3, pinset.D2, pinset.D1, pinset.D0);
+	HD44780_initialize(10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 8, 2, 10);
+	printf("\nEN %d RS %d RW %d D7 %d D6 %d D5 %d D4 %d D3 %d D2 %d D1 %d D0 %d", pinset.EN, pinset.RS, pinset.RW, pinset.D7, pinset.D6, pinset.D5, pinset.D4, pinset.D3, pinset.D2, pinset.D1, pinset.D0);
 	getchar();
 }
 
-void HD44780_pinset(typedefstruct_pinset setpinto) {
+int HD44780_pinset(typedefstruct_pinset setpinto) {
+
+	typedefstruct_pinset save_pinset = pinset;
+	int pinset_error = 0;
 	
-	if (setpinto.EN_pin != 88) {
-		GPIO_SET_OUT("EN", setpinto.EN_pin);
+	pinset = setpinto;
+
+	if (setpinto.D3 == 88 && setpinto.D2 == 88 && setpinto.D1 == 88 && setpinto.D0 == 88) {
+		if (pinset.EN == pinset.RS || pinset.EN == pinset.RW || pinset.EN == pinset.D7 || pinset.EN == pinset.D6 || pinset.EN == pinset.D5 || pinset.EN == pinset.D4 ||
+			pinset.RS == pinset.RW || pinset.RS == pinset.D7 || pinset.RS == pinset.D6 || pinset.RS == pinset.D5 || pinset.RS == pinset.D4 ||
+			pinset.RW == pinset.D7 || pinset.RW == pinset.D6 || pinset.RW == pinset.D5 || pinset.RW == pinset.D4 ||
+			pinset.D7 == pinset.D6 || pinset.D7 == pinset.D5 || pinset.D7 == pinset.D4 ||
+			pinset.D6 == pinset.D5 || pinset.D6 == pinset.D4 ||
+			pinset.D5 == pinset.D4) {
+			pinset_error = 1;
+			printf("doppelte belegung 4bit");
+		}
 	}
-	if (setpinto.RS_pin != 88) {
-		GPIO_SET_OUT("RS", setpinto.RS_pin);
+	else {
+		if (pinset.EN == pinset.RS || pinset.EN == pinset.RW || pinset.EN == pinset.D7 || pinset.EN == pinset.D6 || pinset.EN == pinset.D5 || pinset.EN == pinset.D4 || pinset.EN == pinset.D3 || pinset.EN == pinset.D2 || pinset.EN == pinset.D1 || pinset.EN == pinset.D0 ||
+			pinset.RS == pinset.RW || pinset.RS == pinset.D7 || pinset.RS == pinset.D6 || pinset.RS == pinset.D5 || pinset.RS == pinset.D4 || pinset.RS == pinset.D3 || pinset.RS == pinset.D2 || pinset.RS == pinset.D1 || pinset.RS == pinset.D0 ||
+			pinset.RW == pinset.D7 || pinset.RW == pinset.D6 || pinset.RW == pinset.D5 || pinset.RW == pinset.D4 || pinset.RW == pinset.D3 || pinset.RW == pinset.D2 || pinset.RW == pinset.D1 || pinset.RW == pinset.D0 ||
+			pinset.D7 == pinset.D6 || pinset.D7 == pinset.D5 || pinset.D7 == pinset.D4 || pinset.D7 == pinset.D3 || pinset.D7 == pinset.D2 || pinset.D7 == pinset.D1 || pinset.D7 == pinset.D0 ||
+			pinset.D6 == pinset.D5 || pinset.D6 == pinset.D4 || pinset.D6 == pinset.D3 || pinset.D6 == pinset.D2 || pinset.D6 == pinset.D1 || pinset.D6 == pinset.D0 ||
+			pinset.D5 == pinset.D4 || pinset.D5 == pinset.D3 || pinset.D5 == pinset.D2 || pinset.D5 == pinset.D1 || pinset.D5 == pinset.D0 ||
+			pinset.D4 == pinset.D3 || pinset.D4 == pinset.D2 || pinset.D4 == pinset.D1 || pinset.D4 == pinset.D0 ||
+			pinset.D3 == pinset.D2 || pinset.D3 == pinset.D1 || pinset.D3 == pinset.D0 ||
+			pinset.D2 == pinset.D1 || pinset.D2 == pinset.D0 ||
+			pinset.D1 == pinset.D0) {
+			pinset_error = 1;
+			printf("doppelte belegung 8bit");
+		}
 	}
-	if (setpinto.RW_pin != 88) {
-		GPIO_SET_OUT("RW", setpinto.RW_pin);
+	if (pinset_error == 1) {
+		pinset = save_pinset;
+		return pinset_error;
 	}
-	if (setpinto.D7_pin != 88) {
-		GPIO_SET_OUT("D7", setpinto.D7_pin);
+
+	GPIO_SET_OUT("EN", pinset.EN)
+	GPIO_SET_OUT("RS", pinset.RS)
+	GPIO_SET_OUT("RW", pinset.RW)
+	GPIO_SET_OUT("D7", pinset.D7)
+	GPIO_SET_OUT("D6", pinset.D6)
+	GPIO_SET_OUT("D5", pinset.D5)
+	GPIO_SET_OUT("D4", pinset.D4)
+	if (!(setpinto.D3 == 88 && setpinto.D2 == 88 && setpinto.D1 == 88 && setpinto.D0 == 88)) {
+		GPIO_SET_OUT("D3", pinset.D3)
+		GPIO_SET_OUT("D2", pinset.D2)
+		GPIO_SET_OUT("D1", pinset.D1)
+		GPIO_SET_OUT("D0", pinset.D0)
 	}
-	if (setpinto.D6_pin != 88) {
-		GPIO_SET_OUT("D6", setpinto.D6_pin);
-	}
-	if (setpinto.D5_pin != 88) {
-		GPIO_SET_OUT("D5", setpinto.D5_pin);
-	}
-	if (setpinto.D4_pin != 88) {
-		GPIO_SET_OUT("D4", setpinto.D4_pin);
-	}
-	if (setpinto.D3_pin != 88) {
-		GPIO_SET_OUT("D3", setpinto.D3_pin);
-	}
-	if (setpinto.D2_pin != 88) {
-		GPIO_SET_OUT("D2", setpinto.D2_pin);
-	}
-	if (setpinto.D1_pin != 88) {
-		GPIO_SET_OUT("D1", setpinto.D1_pin);
-	}
-	if (setpinto.D0_pin != 88) {
-		GPIO_SET_OUT("D0", setpinto.D0_pin);
-	}
+
+	return pinset_error;
 }
 
-int HD44780_initialize(char EN_pin, char RS_pin, char RW_pin, char D7_pin, char D6_pin, char D5_pin, char D4_pin, char D3_pin, char D2_pin, char D1_pin, char D0_pin, char funktionset_DL, char funktionset_N, char funktionset_F) {
+int HD44780_initialize(char EN_pin, char RS_pin, char RW_pin, char D7_pin, char D6_pin, char D5_pin, char D4_pin, char D3_pin, char D2_pin, char D1_pin, char D0_pin, char functionset_DL, char functionset_N, char functionset_F) {
 	
 	// GPIO Initialisieren
-		GPIO_INI();	
+		GPIO_INI()
 
 	// Pin einstellungen für 4 Bit Modus
-		if (funktionset_DL == 4) {
-			// Pin überprüfen
-				if (EN_pin == RS_pin || EN_pin == RW_pin || EN_pin == D7_pin || EN_pin == D6_pin || EN_pin == D5_pin || EN_pin == D4_pin ||
-					RS_pin == RW_pin || RS_pin == D7_pin || RS_pin == D6_pin || RS_pin == D5_pin || RS_pin == D4_pin ||
-					RW_pin == D7_pin || RW_pin == D6_pin || RW_pin == D5_pin || RW_pin == D4_pin ||
-					D7_pin == D6_pin || D7_pin == D5_pin || D7_pin == D4_pin ||
-					D6_pin == D5_pin || D6_pin == D4_pin ||
-					D5_pin == D4_pin) {
-					return 0;												// Funktionsabbruch wegen mehrfach Belegung
-				}
-			// Pins auf Output setzen
-				typedefstruct_pinset setpinto = { EN_pin, RS_pin, RW_pin, D7_pin, D6_pin, D5_pin, D4_pin, 88, 88, 88, 88 };
-				HD44780_pinset(setpinto);
+		if (functionset_DL == 4) {
+			typedefstruct_pinset setpinto = { EN_pin, RS_pin, RW_pin, D7_pin, D6_pin, D5_pin, D4_pin, 88, 88, 88, 88 };
+			if (HD44780_pinset(setpinto) == 1) {
+				return 0;
+			}
 		}
 	// Pin einstellungen für 8 Bit Modus
-		else if (funktionset_DL == 8) {
-			// Pins überprüfen
-				if (EN_pin == RS_pin || EN_pin == RW_pin || EN_pin == D7_pin || EN_pin == D6_pin || EN_pin == D5_pin || EN_pin == D4_pin || EN_pin == D3_pin || EN_pin == D2_pin || EN_pin == D1_pin || EN_pin == D0_pin ||
-					RS_pin == RW_pin || RS_pin == D7_pin || RS_pin == D6_pin || RS_pin == D5_pin || RS_pin == D4_pin || RS_pin == D3_pin || RS_pin == D2_pin || RS_pin == D1_pin || RS_pin == D0_pin ||
-					RW_pin == D7_pin || RW_pin == D6_pin || RW_pin == D5_pin || RW_pin == D4_pin || RW_pin == D3_pin || RW_pin == D2_pin || RW_pin == D1_pin || RW_pin == D0_pin ||
-					D7_pin == D6_pin || D7_pin == D5_pin || D7_pin == D4_pin || D7_pin == D3_pin || D7_pin == D2_pin || D7_pin == D1_pin || D7_pin == D0_pin ||
-					D6_pin == D5_pin || D6_pin == D4_pin || D6_pin == D3_pin || D6_pin == D2_pin || D6_pin == D1_pin || D6_pin == D0_pin ||
-					D5_pin == D4_pin || D5_pin == D3_pin || D5_pin == D2_pin || D5_pin == D1_pin || D5_pin == D0_pin ||
-					D4_pin == D3_pin || D4_pin == D2_pin || D4_pin == D1_pin || D4_pin == D0_pin ||
-					D3_pin == D2_pin || D3_pin == D1_pin || D3_pin == D0_pin ||
-					D2_pin == D1_pin || D2_pin == D0_pin ||
-					D1_pin == D0_pin) {
-					return 0;												// Funktionsabbruch wegen mehrfach Belegung
-				}
-				else {
-			// Pins auf Output setzen
-					typedefstruct_pinset setpinto = { EN_pin, RS_pin, RW_pin, D7_pin, D6_pin, D5_pin, D4_pin, D3_pin, D2_pin, D1_pin, D0_pin };
-					HD44780_pinset(setpinto);
-				}
-		}
-	// Pin einstellungen nicht möglich
-		else {														
-
+		else if (functionset_DL == 8) {
+			typedefstruct_pinset setpinto = { EN_pin, RS_pin, RW_pin, D7_pin, D6_pin, D5_pin, D4_pin, D3_pin, D2_pin, D1_pin, D0_pin };
+			if (HD44780_pinset(setpinto) == 1) {
+				return 0;
+			}
 		}
 
-	// Funktionset einstellungen
+	// Functionset einstellungen
 		// DL - Data length
 		/*	
 			Legt die Schnittstellendatenlänge fest. 
 			0 = 4 Bit Modus (DB7 - DB4)
 			1 = 8 Bit Modus (DB7 - DB0) 
 		*/
-			if (funktionset_DL == 4) {
-				funktionset.DL = 0;
+			if (functionset_DL == 4) {
+				functionset.DL = 0;
 			}
-			else if (funktionset_DL == 8) {
-				funktionset.DL = 1;
+			else if (functionset_DL == 8) {
+				functionset.DL = 1;
 			}
 			else {
 				return 0;													// Funktionsabbruch wegen falscher Eingabe
@@ -137,11 +148,11 @@ int HD44780_initialize(char EN_pin, char RS_pin, char RW_pin, char D7_pin, char 
 			0 = 1 Zeile
 			1 = 2 oder 4 Zeilen
 		*/
-			if (funktionset_N == 1) {
-				funktionset.N = 0;
+			if (functionset_N == 1) {
+				functionset.N = 0;
 			}
-			else if (funktionset_N == 2 || funktionset_N == 4) {
-				funktionset.N = 1;
+			else if (functionset_N == 2 || functionset_N == 4) {
+				functionset.N = 1;
 			}
 			else {
 				return 0;													// Funktionsabbruch wegen falscher Eingabe
@@ -152,15 +163,18 @@ int HD44780_initialize(char EN_pin, char RS_pin, char RW_pin, char D7_pin, char 
 			0 = 5x8  Pixel
 			1 = 5x10 Pixel
 		*/
-			if (funktionset_F == 8) {
-				funktionset.F = 0;
+			if (functionset_F == 8) {
+				functionset.F = 0;
 			}
-			else if (funktionset_F == 10) {
-				funktionset.F = 1;
+			else if (functionset_F == 10) {
+				functionset.F = 1;
 			}
 			else {
 				return 0;													// Funktionsabbruch wegen falscher Eingabe
 			}
+
+		// Daten an HD44780 senden
+
 		
 	return 0;
 }
--- HD44780.h/HD44780.h
+++ HD44780.h/HD44780.h
@@ -15,77 +15,85 @@
 
 //	printfs
 #   define PRINT_TEST                   printf("\n[ TEST ] ");
+#	define PRINT_ERROR					printf("\n[ ERROR] ");
 #	define PRINT_FAILED					printf("\n[FAILED] ");
 #	define PRINT_OK						printf("\n[  OK  ] ");
 
 			
-// printfs lang
+
 // DE
-#	define PRINT_GPIO_INI				printf("%s - Initialisieren", GPIO_H);
-#	define PRINT_GPIO_SET_OUT(port,pin)	printf("%s - Setze Pin %2i (%s) auf OUTPUT", GPIO_H, pin, port); 
-#	define PRINT_GPIO_SET_IN(port,pin)	printf("%s - Setze Pin %2i (%s) auf INPUT", GPIO_H, pin, port); 
+	// printfs lang
+#		define PRINT_GPIO_INI				printf("%s - Initialisieren", GPIO_H);
+#		define PRINT_GPIO_SET_OUT(port,pin)	printf("%s - Setze Pin %2i (%s) auf OUTPUT", GPIO_H, pin, port); 
+#		define PRINT_GPIO_SET_IN(port,pin)	printf("%s - Setze Pin %2i (%s) auf INPUT", GPIO_H, pin, port); 
+	// ERRORs
 
+#		define PRINT_ERROR_MULTIPLEPINS		printf("Einstellungsfehler - Mehrfache Pinbelegung");
 
-// 1 PIGPIO
+// GPIO
 #	ifdef PIGPIO_H
+	// 1 PIGPIO
 #		include "pigpio.h"
 #
-#   	define GPIO_INI()           if (gpioInitialise() < 0){PRINT_FAILED}else{PRINT_OK};; PRINT_GPIO_INI;
+#   	define GPIO_INI()           if (gpioInitialise() < 0){PRINT_FAILED}else{PRINT_OK}; PRINT_GPIO_INI
 #
-#	    define GPIO_SET_OUT(pin)    gpioSetMode(pin, PI_OUTPUT); if(gpioGetMode(pin)!=PI_OUTPUT){PRINT_FAILED}else{PRINT_OK}; PRINT_GPIO_SET_OUT(port,pin);
-#	    define GPIO_SET_IN(pin)	    gpioSetMode(pin, PI_INPUT); if(gpioGetMode(pin)!=PI_INPUT){PRINT_FAILED}else{PRINT_OK}; PRINT_GPIO_SET_IN(port,pin);
+#	    define GPIO_SET_OUT(pin)    gpioSetMode(pin, PI_OUTPUT); if(gpioGetMode(pin)!=PI_OUTPUT){PRINT_FAILED}else{PRINT_OK}; PRINT_GPIO_SET_OUT(port,pin)
+#	    define GPIO_SET_IN(pin)	    gpioSetMode(pin, PI_INPUT); if(gpioGetMode(pin)!=PI_INPUT){PRINT_FAILED}else{PRINT_OK}; PRINT_GPIO_SET_IN(port,pin)
 #  		define GPIO_OFF(pin)        gpioWrite(pin, 0);
 #    	define GPIO_ON(pin)         gpioWrite(pin, 1);
 #		define GPIO_X(pin)
 #	else
 
-// 0 NONE
+	// 0 NONE
 #		define GPIO_H "NONE"
 #		// no include 
 #
-#	    define GPIO_INI()				PRINT_TEST; PRINT_GPIO_INI;
+#	    define GPIO_INI()				PRINT_TEST PRINT_GPIO_INI
 #
-#		define GPIO_SET_OUT(port,pin)	PRINT_TEST; PRINT_GPIO_SET_OUT(port,pin);
-#   	define GPIO_SET_IN(port,pin)	PRINT_TEST; PRINT_GPIO_SET_IN(port,pin);
-#    	define GPIO_OFF(pin)			PRINT_TEST; printf("Pin %i ausschalten", pin);
-#		define GPIO_ON(pin)				PRINT_TEST; printf("Pin %i einschalten", pin);
+#		define GPIO_SET_OUT(port,pin)	PRINT_TEST PRINT_GPIO_SET_OUT(port,pin)
+#   	define GPIO_SET_IN(port,pin)	PRINT_TEST PRINT_GPIO_SET_IN(port,pin)
+#    	define GPIO_OFF(pin)			PRINT_TEST printf("Pin %i ausschalten", pin)
+#		define GPIO_ON(pin)				PRINT_TEST printf("Pin %i einschalten", pin)
 #
 #	endif // GPIO_H == NONE
 
-// default Werte
+// ERRORs
 
-#define DEFAULT_FUNKTIONSET_DL 1
-#define DEFAULT_FUNKTIONSET_N 1
-#define DEFAULT_FUNKTIONSET_F 0
+# define ERROR_MULTIPLEPINS				PRINT_ERROR; PRINT_ERROR_MULTIPLEPINS;
+
+
 
 /*--- Strukturdefinitionen und Variablen ------------------------------*/
 
-typedef struct typedefstruct_pinset {
-	char EN_pin;
-	char RS_pin;
-	char RW_pin;
-	char D7_pin;
-	char D6_pin;
-	char D5_pin;
-	char D4_pin;
-	char D3_pin;
-	char D2_pin;
-	char D1_pin;
-	char D0_pin;
+typedef struct {
+	char EN;
+	char RS;
+	char RW;
+	char D7;
+	char D6;
+	char D5;
+	char D4;
+	char D3;
+	char D2;
+	char D1;
+	char D0;
 }typedefstruct_pinset;
 
 typedef struct {
 	char DL;						// 0 : 4 Bit Modus	| 1 : 8 Bit Modus
 	char N;							// 0 : 1 Zeile		| 1 : 2 Zeilen
 	char F;							// 0 : 5x8 Pixel	| 1 : 5x10 Pixel
-}typedefstruct_funktionset;
+}typedefstruct_functionset;
 
 
 /*--- Variablendefinitionen ------------------------------------------*/
 
 
 /*--- Funktionsdefinitionen ------------------------------------------*/
-extern void HD44780_pinset(typedefstruct_pinset);
 extern int HD44780_initialize(char, char, char, char, char, char, char, char, char, char, char, char, char, char);
+extern int HD44780_pinset(typedefstruct_pinset);
+extern void HD44780_functionset(typedefstruct_functionset);
+
+
 
 #endif // HD44780_H
